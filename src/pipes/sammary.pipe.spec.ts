6e3da564a632758cf5c8c7f5b6461d2568200a34
rabraghib <49442862+rabraghib@users.noreply.github.com>
2020-05-16 15:47:18 +0000

    delet

--- src/pipes/sammary.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SammaryPipe } from './sammary.pipe';
-
-describe('SammaryPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SammaryPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
